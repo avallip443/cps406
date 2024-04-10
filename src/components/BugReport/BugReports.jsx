6f42384aa2822d58dbea93913f6ff457ad0f0f31
Arathi Vallipuranathan <avallipuranathan@gmail.com>
2024-04-10 13:04:58 -0400

    updated table for bug reports

--- src/components/BugReport/BugReports.jsx
+++ src/components/BugReport/BugReports.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import {
+  Container,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Spinner,
+} from "@chakra-ui/react";
+import useGetBugReports from "../../hooks/getBugReports";
 
 const BugReports = () => {
-  return (
-    <div>BugReports</div>
-  )
-}
+  const { isLoading, reports } = useGetBugReports();
 
-export default BugReports
+  return (
+    <Container maxW={"container.md"}>
+      {isLoading ? (
+        <Spinner size="xl" />
+      ) : (
+        <Table variant={"striped"} colorScheme={"teal"} size={"md"}>
+          <Thead>
+            <Tr>
+              <Th>Bug Name</Th>
+              <Th>Description</Th>
+              <Th>Priority Level</Th>
+              <Th>Status</Th>
+              <Th>Created At</Th>
+              <Th>User</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {reports?.map((report) => (
+              <Tr key={report.id}>
+                <Td>{report.bugName}</Td>
+                <Td>{report.bugDescription}</Td>
+                <Td>{report.priorityLevel}</Td>
+                <Td>{report.bugStatus}</Td>
+                <Td>{new Date(report.createdAt).toLocaleString()}</Td>
+                <Td>{report.userName}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      )}
+    </Container>
+  );
+};
+
+export default BugReports;
