abeb9aa1b1c3ba41d5c04a05cfe7c358c5c23a68
Arathi Vallipuranathan <avallipuranathan@gmail.com>
2024-04-10 20:24:09 -0400

    update input styling

--- src/components/AuthForm/Login.jsx
+++ src/components/AuthForm/Login.jsx
@@ -3,24 +3,21 @@ import { useState } from "react";
 import useLogin from "../../hooks/useLogin";
 
 const Login = () => {
-  // NOTE: this page has all the elements and function related to logging in
-
-  // NOTE: this sets the inital values of the user's credentials (empty atm)
   const [inputs, setInputs] = useState({
     email: "",
     password: "",
     confirmPassword: "",
   });
 
-  // NOTE: gets login function from useLogin() hook
   const { loading, error, login } = useLogin();
 
   return (
     <>
-      {/* TODO: style elements*/}
       <Input
         size={"sm"}
         placeholder="Email"
+        _placeholder={{ color: "gray.300" }}
+        borderColor={"gray.300"}
         type="email"
         value={inputs.email}
         onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
@@ -29,11 +26,13 @@ const Login = () => {
       <Input
         size={"sm"}
         placeholder="Password"
+        _placeholder={{ color: "gray.300" }}
+        borderColor={"gray.300"}
         type="password"
         value={inputs.password}
         onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
       />
-       
+
       {/* displays errors with logging in using error from login hook */}
       {error && (
         <Alert status="error" borderRadius={4} fontSize={13} p={2}>
@@ -42,7 +41,6 @@ const Login = () => {
         </Alert>
       )}
 
-      {/* NOTE: onClick sends the inputted information to the useLogin function!! */}
       <Button
         colorScheme="pink"
         isLoading={loading}
@@ -51,7 +49,9 @@ const Login = () => {
         Log in
       </Button>
 
-      <Text _hover={{color:"blue.700"}} as="u">Forgot Password?</Text>
+      <Text _hover={{ color: "blue.700" }} as="u">
+        Forgot Password?
+      </Text>
     </>
   );
 };
