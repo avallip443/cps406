7040ca748c3137aa7b766e93f6397988fc0e7271
carole-youssef <carole.youssef@torontomu.ca>
2024-04-11 13:08:06 -0400

    report design fixed

--- src/components/BugReport/ReportForm.jsx
+++ src/components/BugReport/ReportForm.jsx
@@ -10,6 +10,8 @@ import {
   Input,
   Radio,
   RadioGroup,
+  Center,
+  Container,
 } from "@chakra-ui/react";
 import { collection, addDoc } from "firebase/firestore";
 import { firestore } from "../../firebase/firebase";
@@ -60,78 +62,89 @@ const ReportForm = () => {
 
   return (
     <>
-      <Box bg={"#e37ab1"} w='100%' p={4} color='white'>
-        <Heading size='xl'>Report a Bug</Heading>
+    <Center h="100vh" w="200vh">
+      <Box bg={"pink.200"} w='100%' p={4} color='white'>
+        <Heading size="xl" textAlign="center" mb={8} color={"#0B1957"}>Report a Bug</Heading>
         <form onSubmit={handleSubmit}>
         <FormControl isRequired mt={8}>
-            <FormLabel>Your name</FormLabel>
+            <FormLabel color={"#0B1957"}>Your name</FormLabel>
+            <Box bg="#fa9ebc" borderRadius={"3px"}>
             <Input
               type="text"
               value={userName}
               onChange={(e) => setUserName(e.target.value)}
               placeholder="e.g. John Doe"
+              _placeholder={{color: "white"}}
             />
+            </Box>
           </FormControl>
 
           <FormControl isRequired mt={8}>
-            <FormLabel>Bug Name</FormLabel>
+            <FormLabel color={"#0B1957"}>Bug Name</FormLabel>
+            <Box bg="#fa9ebc" borderRadius={"3px"}>
             <Input
               type="text"
               value={bugName}
               onChange={(e) => setBugName(e.target.value)}
               placeholder="What will you call this bug?"
+              _placeholder={{color: "white"}}
             />
+            </Box>
           </FormControl>
 
           <FormControl as="fieldset" isRequired mt={8}>
-            <FormLabel as="legend">Priority Level</FormLabel>
+            <FormLabel as="legend" color={"#0B1957"}>Priority Level</FormLabel>
             <RadioGroup
               defaultValue="Low"
               value={priorityLevel}
               onChange={(value) => setPriorityLevel(value)}
-              colorScheme='teal'
+              color='#0B1957'
             >
-              <HStack spacing="62px">
+              <HStack spacing="62px" color={"#0B1957"}>
                 <Radio value="Low">Low</Radio>
                 <Radio value="Medium">Medium</Radio>
                 <Radio value="High">High</Radio>
               </HStack>
             </RadioGroup>
-            <FormHelperText>Select the priority level.</FormHelperText>
+            <FormHelperText color={"#0B1957"}>Select the priority level.</FormHelperText>
           </FormControl>
 
           <FormControl isRequired mt={8}>
-            <FormLabel>Bug Description</FormLabel>
+            <FormLabel color={"#0B1957"}>Bug Description</FormLabel>
+            <Box bg="#fa9ebc" borderRadius={"3px"}>
             <Input
               type="text"
               value={bugDescription}
               onChange={(e) => setBugDescription(e.target.value)}
               placeholder='Please describe the bug in full detail...'
+              _placeholder={{color: "white"}}
             />
+            </Box>
           </FormControl>
 
           <FormControl as="fieldset" isRequired mt={8}>
-            <FormLabel as="legend">Status</FormLabel>
+            <FormLabel as="legend" color={"#0B1957"}>Status</FormLabel>
             <RadioGroup
               defaultValue="Active"
               value={bugStatus}
               onChange={(value) => setBugStatus(value)}
-              colorScheme='teal'
+              color='#0B1957'
             >
-              <HStack spacing="62px">
+              <HStack spacing="62px" color={"#0B1957"}>
                 <Radio value="Active">Active</Radio>
                 <Radio value="Closed">Closed</Radio>
                 <Radio value="Fixed">Fixed</Radio>
               </HStack>
             </RadioGroup>
-            <FormHelperText>Select status of bug.</FormHelperText>
+            <FormHelperText color={"#0B1957"}>Select status of bug.</FormHelperText>
           </FormControl>
 
-          <Button mt={12} colorScheme="teal" type="submit" color='white'>
+          <Button mt={12} colorScheme="blue" variant="solid" color= "#0B1957">
             Submit
           </Button>
         </form>
       </Box>
+      </Center>
     </>
   );
 };
